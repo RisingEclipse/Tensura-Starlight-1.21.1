aeeba1760ee3c43377f122ba114d4d609a44ad38
PropheticEclipse <Kokororinaaal@gmail.com>
2026-01-06 11:54:40 -0600

    Added in Light Remains Lang Files, Ported in damage multis (now fixed, and functional), ported magic sense acquirement (now properly costless!), Now properly acquires when learned, now refunds cost when obtained while owning Light/Darkness lord.

--- src/main/java/com/github/propheticeclipse/tensurastarlight/ability/skill/unique/LightRemainsSkill.java
+++ src/main/java/com/github/propheticeclipse/tensurastarlight/ability/skill/unique/LightRemainsSkill.java
@@ -1,14 +1,24 @@
 package com.github.propheticeclipse.tensurastarlight.ability.skill.unique;
 
+import com.github.propheticeclipse.tensurastarlight.registry.skills.StarlightUniqueSkills;
+import io.github.manasmods.manascore.network.api.util.Changeable;
+import io.github.manasmods.manascore.skill.api.EntityEvents;
 import io.github.manasmods.manascore.skill.api.ManasSkillInstance;
+import io.github.manasmods.tensura.ability.SkillHelper;
+import io.github.manasmods.tensura.ability.SkillUtils;
+import io.github.manasmods.tensura.ability.TensuraSkillInstance;
 import io.github.manasmods.tensura.ability.magic.Element;
 import io.github.manasmods.tensura.ability.skill.Skill;
+import io.github.manasmods.tensura.damage.TensuraDamageHelper;
+import io.github.manasmods.tensura.registry.skill.ExtraSkills;
 import io.github.manasmods.tensura.storage.TensuraStorages;
 import io.github.manasmods.tensura.util.EnergyHelper;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
 
 public class LightRemainsSkill extends Skill {
 
@@ -29,16 +39,25 @@ public class LightRemainsSkill extends Skill {
     }
 
     @Override
+    public int getAcquirementMastery(LivingEntity entity) {
+        return 1;
+    }
+
+    @Override
     public void onLearnSkill(ManasSkillInstance instance, LivingEntity entity) {
-        if (TensuraStorages.getSpiritFrom(entity).getSpiritLevelId(Element.LIGHT) >= 4) {
+        CompoundTag tag = new CompoundTag();
+        boolean LightLord = (TensuraStorages.getSpiritFrom(entity).getSpiritLevelId(Element.LIGHT) >= 4);
+        boolean DarkLord = (TensuraStorages.getSpiritFrom(entity).getSpiritLevelId(Element.DARKNESS) >= 4);
+        if (LightLord || DarkLord) {
             double maxMagicule = EnergyHelper.getMaxMagicule(entity);
             double acquireCost = getAcquiringMagiculeCost(instance);
             EnergyHelper.setMaxMagicule(entity, (maxMagicule + acquireCost));
         }
-        if (TensuraStorages.getSpiritFrom(entity).getSpiritLevelId(Element.DARKNESS) >= 4) {
-            double maxMagicule = EnergyHelper.getMaxMagicule(entity);
-            double acquireCost = getAcquiringMagiculeCost(instance);
-            EnergyHelper.setMaxMagicule(entity, (maxMagicule + acquireCost));
+
+        if (!SkillUtils.hasSkillFully(entity, ExtraSkills.MAGIC_SENSE.get())) {
+            tag.putBoolean("NoMagiculeCost", true);
+            SkillHelper.learnSkill(entity, ExtraSkills.MAGIC_SENSE.get());
+            tag.putBoolean("NoMagiculeCost", false);
         }
     }
 
@@ -76,7 +95,40 @@ public class LightRemainsSkill extends Skill {
         }
     }
 
-    public void onPressed(ManasSkillInstance instance, LivingEntity entity, int keyNumber, int mode) {
+    @Override
+    public boolean canBeToggled(ManasSkillInstance instance, LivingEntity entity) {
+        return instance.isMastered(entity);
+    }
 
+    public boolean onDamageEntity(ManasSkillInstance instance, LivingEntity owner, LivingEntity target, DamageSource source, Changeable<Float> amount) {
+        // In Slot]: 1.5x (3x Mastered) Light Damage. Unmastered effect is toggleable when mastered.
+        CompoundTag tag = instance.getOrCreateTag();
+        double damageBonus = isMastered(instance, owner) ? 3.0 : 1.5; // Mastered : Unmastered
+        if (this.isInSlot(owner, instance)) {
+            if (TensuraDamageHelper.isLightDamage(source)) {
+                Float initDamage = amount.get();
+                if (initDamage != null) {
+                    amount.set( (float) (initDamage * damageBonus));
+                }
+            }
+        } else if ((instance.isToggled()) && instance.isMastered(owner)) {
+            if (TensuraDamageHelper.isLightDamage(source)) {
+                Float initDamage = amount.get();
+                if (initDamage != null) {
+                    amount.set((float) (initDamage * 1.5));
+                }
+            }
+        } return true;
+    }
+
+    public void onPressed(ManasSkillInstance instance, LivingEntity entity, int keyNumber, int mode) {
+        switch (instance.getModes()) {
+            case 0:
+                break;
+            case 1:
+                break;
+            default:
+                break;
+        }
     }
 }
